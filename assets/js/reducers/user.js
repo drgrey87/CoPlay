ece7e256e0d6a6c0f3dcccf359e0a48b264877eb
sergey <dr.grey_101by@inbox.ru>
2016-06-23 00:02:21 +0300

    changed boilerplate

--- assets/js/reducers/user.js
+++
@@ -1,7 +0,0 @@
-const initialState = {
-  user: 'sergey'
-}
-
-export default function user (state = initialState) {
-  return state;
-}
--- tasks/register/run.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (grunt) {
-  grunt.registerTask('run', [
-    'webpack',
-    'clean',
-    'copy',
-    'nodemon',
-    'watch'
-  ]);
-};
