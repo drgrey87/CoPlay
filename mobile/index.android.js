ee4c4abbde8db8b07db4fc1f1e60356db285a160
sergey <dr.grey_101by@inbox.ru>
2017-02-05 18:44:32 +0300

    simple app

--- mobile/index.android.js
+++ mobile/index.android.js
@@ -10,18 +10,54 @@ import {
   StyleSheet,
   Text,
   View,
-  Dimensions
+  DrawerLayoutAndroid,
+  ToolbarAndroid
 } from 'react-native';
-import Map from './app/Map';
+import App from './app/components/App'
 
 export default class mobile extends Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Map/>
-      </View>
+      <App />
     );
   }
+
+  // render() {
+  //   return (
+  //     <ToolbarAndroid
+  //       logo={require('./app/assets/1.png')}
+  //       style={{backgroundColor: 'red', height:50}}
+  //       title="AwesomeApp"
+  //       actions={[{title: 'Settings', icon: require('./app/assets/2.png'), show: 'always'}]}
+  //       onActionSelected={this.onActionSelected} />
+  //   )
+  // }
+  //
+  // onActionSelected(position) {
+  //   if (position === 0) { // index of 'Settings'
+  //     console.log(1111);
+  //   }
+  // }
+
+  // render() {
+  //   var navigationView = (
+  //     <View style={{flex: 1, backgroundColor: '#e5e5e5'}}>
+  //       <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
+  //     </View>
+  //   );
+  //   return (
+  //     <DrawerLayoutAndroid
+  //       drawerWidth={300}
+  //       drawerPosition={DrawerLayoutAndroid.positions.Left}
+  //       renderNavigationView={() => navigationView}>
+  //       <View style={{flex: 1, alignItems: 'center'}}>
+  //         <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
+  //         <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
+  //       </View>
+  //     </DrawerLayoutAndroid>
+  //   );
+  // }
 }
 
 const styles = StyleSheet.create({
